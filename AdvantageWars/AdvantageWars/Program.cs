05a9b4ffa8a9fce3f171bbfcb7cdc502955d7c81
lucasloizzo <loizzolucas@gmail.com>
2021-07-21 10:14:20 -0300

    initial commit

---
+++ AdvantageWars/AdvantageWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvantageWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
